fc7edc5e525c0dd786d20b571ff05740309a8a68
Michael Furlong <furlong@cmu.edu>
2019-06-11 11:51:01 -0700

    Added python to script to send and receive messages forever and ever and ever

---
+++ src/examples/SUPERball/AppSUPERSend.py
@@ -0,0 +1,25 @@
+import zmq
+import time
+import numpy as np
+
+def main():
+    port = 5555 
+
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:%s" % port)
+
+    msg = ','.join(['10000' for _ in range(25)])
+
+
+#     for _ in range(200):
+    while True:
+        socket.send(msg)
+        message = socket.recv()
+        print(message)
+
+
+### end main    
+
+if __name__ == '__main__':
+    main()
