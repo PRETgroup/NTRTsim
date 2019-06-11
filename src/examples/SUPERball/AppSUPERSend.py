a124a54bd5105c62d59f8cca83ede045fdcd385c
kiwih <hammondpearce@outlook.com>
2019-06-12 07:25:16 +1200

    Python slump/deslump

--- src/examples/SUPERball/AppSUPERSend.py
+++ src/examples/SUPERball/AppSUPERSend.py
@@ -3,6 +3,7 @@
 import zmq
 import time
 import numpy as np
+import math
 
 def main():
     port = 5555 
@@ -11,14 +12,20 @@ def main():
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:%s" % port)
 
-    msg = ','.join(['10000' for _ in range(25)])
-
+    lifetime = 0.0
 
 #     for _ in range(200):
     while True:
+        tr = math.sin(2*math.pi*(0.25)*lifetime)+1.0
+
+        msg = ','.join([str(tr*10000) for _ in range(25)])
+
         socket.send_string(msg)
         message = socket.recv()
-        print(message)
+        
+        lifetime = float(message)
+        print(lifetime)
+        
 
 
 ### end main    
